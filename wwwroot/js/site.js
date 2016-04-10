81bb168a43a4896c48d8a3d49ac8e317b64bc89d
Kevin Pilch-Bisson <kevin@pilch-bisson.net>
2016-04-09 18:25:26 -0700

    Make the table sortable

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,7 @@
 ﻿// Write your Javascript code.
+$('.sorted_table').sortable({
+  containerSelector: 'table',
+  itemPath: '> tbody',
+  itemSelector: 'tr',
+  placeholder: '<tr class="placeholder"/>'
+});
